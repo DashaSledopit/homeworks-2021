7123e31dba5ac04d4992a10968fd07db8aa9adab
Dasha Sledopit <nastjakireeva666exit!>
2021-04-21 10:50:32 +0300

    Create runner.rb

--- HW02/Nastja_Kireeva/array_module.rb
+++ HW02/Nastja_Kireeva/array_module.rb
@@ -54,14 +54,3 @@ module ArrayModule
     end
   end
 end
-
-using ArrayModule
-
-array = [1, 2, 3, 4, 5, 6]
-
-array.my_each { |element| p element }
-p array.my_map { |element| element * 3 }
-p array.my_select(&:odd?)
-p array.my_reject(&:odd?)
-p array.my_empty?
-p array.my_count { |element| (element % 3).zero? }
