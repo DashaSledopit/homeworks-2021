d89dd034d165e3859373c2968951d6d8dd02f418
Dasha Sledopit <nastjakireeva666exit!>
2021-04-27 21:59:56 +0300

    Create method generating an array containing information about post requests

---
+++ HW03/Nastja_Kireeva/Nastja_Kireeva_hw_03_t_02.rb
@@ -0,0 +1,21 @@
+DATETIME = /(?<=\[).*(?=\])/.freeze
+IP = /^\d{2}.\d{1}.\d{3}.\d+/.freeze
+ADDRESS = %r{/\w+/\d+/\w+}.freeze
+
+def task2(logs)
+  post_requests = []
+  lines = logs.split(/\n/)
+
+  lines.map do |line|
+    if line.include?('POST')
+      datetime = line.scan(DATETIME).first
+      address = line.scan(ADDRESS).first.upcase
+      ip = line.scan(IP).first
+      post_requests << "#{datetime} FROM: #{ip} TO: #{address}"
+    else
+      []
+    end
+  end
+
+  p post_requests
+end
