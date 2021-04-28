081429f052996b8d3d8e2688566264bf0ab677ed
Dasha Sledopit <nastjakireeva666exit!>
2021-04-28 23:21:41 +0300

    Fix calculating seconds between several events

--- HW03/Nastja_Kireeva/Nastja_Kireeva_hw_03_t_03.rb
+++ HW03/Nastja_Kireeva/Nastja_Kireeva_hw_03_t_03.rb
@@ -1,5 +1,4 @@
 require 'time'
-
 TIMESTAMP = /^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d/.freeze
 
 def task3(logs)
@@ -14,7 +13,9 @@ def task3(logs)
 end
 
 def calculate_diff(events)
-  event1 = Time.parse(events[0])
-  event2 = Time.parse(events[1])
-  diff = (event2 - event1).to_s
+  diff = []
+  events.each_cons(2) do |previous_event, next_event|
+    diff << Time.parse(next_event) - Time.parse(previous_event)
+  end
+  diff.join(',')
 end
