70cf5076e5689cd8d3a59f8903c99113b8bc93d3
Dasha Sledopit <nastjakireeva666exit!>
2021-04-29 22:37:03 +0300

    Remove useless variable in task2

--- HW03/Nastja_Kireeva/Nastja_Kireeva_hw_03_t_02.rb
+++ HW03/Nastja_Kireeva/Nastja_Kireeva_hw_03_t_02.rb
@@ -3,15 +3,12 @@ IP = /^\d{2}.\d{1}.\d{3}.\d+/.freeze
 ADDRESS = %r{/\w+/\d+/\w+}.freeze
 
 def task2(logs)
-  post_requests = []
   lines = logs.split(/\n/)
 
   lines.select { |line| line.include?('POST') }.map do |line|
     datetime = line.scan(DATETIME).first
     address = line.scan(ADDRESS).first.upcase
     ip = line.scan(IP).first
-    post_requests << "#{datetime} FROM: #{ip} TO: #{address}"
+    "#{datetime} FROM: #{ip} TO: #{address}"
   end
-
-  post_requests
 end
